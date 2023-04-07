07c7f35e6f0c07b3b28ef260f66841c99760c4e6
shah-jamal <jamalweb007@gmail.com>
2023-04-07 09:39:50 +0600

    JavaScript event for comment Box

---
+++ js/event.js
@@ -0,0 +1,14 @@
+
+// document.getElementById('post-comments').addEventListener('click', function(){
+
+//     const commentBox = document.getElementById('new-comments');
+//     //const commentVelue = commentBox.velue;
+
+//     const newComments = document.createElement('p');
+//     newComments.innerText = commentBox.Value;
+
+//     const commentContainer = document.getElementById('comments-container');
+//     commentContainer.appendChild(newComments);
+
+//     commentBox.Value = '';
+// })
